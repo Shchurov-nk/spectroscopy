de27d7cc98da35e53455e54f07713f95c12e8d98
Shchurov Nickolay <shchurov_no2@mail.ru>
2025-11-10 14:03:11 +0300

    first working main script

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from code.utils.config import load_config
 config = load_config()
-from code.data.preprocess import DataProcessor
+from code.data.dataset import DataProcessor
+from code.data.features import FeatureSelector
 
 data_processor = DataProcessor(config.data)
 data_processor.process_raw_data()
+
+feature_selector = FeatureSelector(config.data)
+feature_selector.calculate_correlations()
